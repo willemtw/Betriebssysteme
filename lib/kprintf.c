456a72291b45258c5c3f7c92e194013a49274345
mclrc <moritzamando@proton.me>
2025-11-05 18:23:44 +0100

    Fixes & cleanup

--- lib/kprintf.c
+++ lib/kprintf.c
@@ -2,7 +2,7 @@
 #include <arch/bsp/uart.h>
 #include <stdarg.h>
 
-static void convert(unsigned int value, int base, int width, char pad)
+static void print_in_base(unsigned int value, int base, int width, char pad)
 {
 	char buffer[16];
 	int  i = 0;
@@ -20,27 +20,83 @@ static void convert(unsigned int value, int base, int width, char pad)
 	}
 }
 
+uint8_t check_for_unknown_format(const char *format)
+{
+	while (*format) {
+		while (*format != 0 && *format != '%') {
+			format++;
+		}
+		if (*format == 0) {
+			return 0;
+		}
+		format++;
+		if (*format == 0) {
+			return 0;
+		}
+		if (*format == '0') {
+			format++;
+			if (format == 0) {
+				return 0;
+			}
+		}
+		if (*format >= '0' && *format <= '9') {
+			format++;
+			if (format == 0) {
+				return 0;
+			}
+		}
+		switch (*format) {
+		case 'c':
+		case 's':
+		case 'i':
+		case 'u':
+		case 'x':
+		case 'p':
+		case '%': {
+			format++;
+			continue;
+		}
+		default:
+			return 1;
+		}
+	}
+	return 0;
+}
+
 void kprintf(const char *format, ...)
 {
 	va_list args;
 	va_start(args, format);
 
+	if (check_for_unknown_format(format)) {
+		kprintf("Unknown conversion specifier\n");
+		return;
+	}
 	while (*format) {
 		if (*format != '%') {
 			uart_putc(*format++);
 			continue;
 		}
 		format++;
+		if (*format == 0) {
+			return;
+		}
 
 		char pad   = ' ';
 		int  width = 0;
 		if (*format == '0') {
 			pad = '0';
 			format++;
+			if (*format == 0) {
+				return;
+			}
 		}
 		if (*format >= '0' && *format <= '9') {
 			width = *format - '0';
 			format++;
+			if (*format == 0) {
+				return;
+			}
 		}
 
 		switch (*format) {
@@ -62,17 +118,17 @@ void kprintf(const char *format, ...)
 				uart_putc('-');
 				val = -val;
 			}
-			convert(val, 10, width, pad);
+			print_in_base(val, 10, width, pad);
 			break;
 		}
 		case 'u': {
 			unsigned int val = va_arg(args, unsigned int);
-			convert(val, 10, width, pad);
+			print_in_base(val, 10, width, pad);
 			break;
 		}
 		case 'x': {
 			unsigned int val = va_arg(args, unsigned int);
-			convert(val, 16, width, pad);
+			print_in_base(val, 16, width, pad);
 			break;
 		}
 		case 'p': {
@@ -81,7 +137,7 @@ void kprintf(const char *format, ...)
 			width		 = 8;
 			pad		 = '0';
 			unsigned int val = va_arg(args, unsigned int);
-			convert(val, 16, width, pad);
+			print_in_base(val, 16, width, pad);
 			break;
 		}
 		case '%': {
@@ -89,7 +145,7 @@ void kprintf(const char *format, ...)
 			break;
 		}
 		default: {
-			kprintf("Unknown format specifier\n");
+			kprintf("Unknown conversion specifier\n");
 		}
 		}
 		format++;
