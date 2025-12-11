afec0f1107ca2b5eccf624c1116493a79eb6be66
mclrc <moritzamando@proton.me>
2025-12-11 14:11:46 +0100

    dummy commit

--- kernel/start.c
+++ kernel/start.c
@@ -10,6 +10,21 @@
 #include <tests/exceptions.h>
 #include <user/main.h>
 
+//struct test_arg {
+//	uint32_t    a;
+//	uint8_t	    b;
+//	uint16_t    c;
+//	const char *name;
+//	uint32_t    d;
+//};
+//
+//void arg_test_thread(void *arg_void)
+//{
+//	struct test_arg *arg = (struct test_arg *)arg_void;
+//	kprintf("a: %u,\nb: %u,\nc: %u,\nname: %s,\nd: %u", arg->a, arg->b, arg->c, arg->name,
+//		arg->d);
+//}
+
 void start_kernel [[noreturn]] (void)
 {
 	uart_init();
@@ -19,5 +34,15 @@ void start_kernel [[noreturn]] (void)
 	kprintf("=== Betriebssystem gestartet ===\n");
 	test_kernel();
 
+//	struct test_arg arg = {
+//		.a    = 27,
+//		.b    = 10,
+//		.c    = 0xff,
+//		.name = "This is a name",
+//		.d    = 0b101010101,
+//	};
+//
+//	scheduler_thread_create(arg_test_thread, &arg, sizeof(arg));
+//
 	scheduler_start();
 }
